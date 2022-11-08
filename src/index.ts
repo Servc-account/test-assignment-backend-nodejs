499c1c5cf43a527d1fb90df2dadcec0eb8dec450
Yanislav Igonin <yanislavdabest@gmail.com>
2022-11-08 18:06:21 +0200

    add router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import Koa from 'koa';
-const app = new Koa();
+import Router from '@koa/router';
 
+const app = new Koa();
+const router = new Router();
 const PORT = 3000;
+
+router.get('/', (ctx) => {
+  ctx.body = { sup: 'bitches' };
+});
 
 app.use(async (ctx, next) => {
   await next();
@@ -16,9 +22,7 @@ app.use(async (ctx, next) => {
   ctx.set('X-Response-Time', `${ms}ms`);
 });
 
-app.use(async ctx => {
-  ctx.body = { sup: 'bitches' };
-});
+app.use(router.routes());
 
 app.listen(PORT);
 console.log(`listening port ${PORT}`);
