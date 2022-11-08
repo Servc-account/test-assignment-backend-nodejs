39b3b977a62eb227b3ba01244390d06ad37b05cf
Yanislav Igonin <yanislavdabest@gmail.com>
2022-11-08 18:04:31 +0200

    add basic koa server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('lol');
+import Koa from 'koa';
+const app = new Koa();
+
+const PORT = 3000;
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+app.use(async ctx => {
+  ctx.body = { sup: 'bitches' };
+});
+
+app.listen(PORT);
+console.log(`listening port ${PORT}`);
